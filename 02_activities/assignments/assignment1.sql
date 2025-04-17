3b6430b83034aff9be94064ed391f9a28f16c6ec
deysoumyadey <dey.soumyadey@gmail.com>
2025-04-17 00:10:48 -0400

    Update

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -12,7 +12,8 @@ sorted by customer_last_name, then customer_first_ name. */
 
 SELECT * 
 FROM customer 
-ORDER BY customer_last_name, customer_first_name;
+ORDER BY customer_last_name, customer_first_name
+LIMIT 10;
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
